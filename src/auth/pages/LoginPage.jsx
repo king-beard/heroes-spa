87807f2a58d436a0636185716efe03f385665439
king-beard <chucho_herrera10@icloud.com>
2023-08-11 15:09:37 -0600

    useNavigate

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,10 +1,21 @@
 import { Fragment } from "react"
+import { useNavigate } from "react-router-dom"
 
 export const LoginPage = () => {
+
+  const navigate = useNavigate()
+
+  const onLogin = () => {
+    navigate("/marvel", { replace : true })
+  }
+
   return (
-    <Fragment>
+    <div className="container mt-5">
       <h1>Login Page</h1>
-    </Fragment>
+      <hr />
+
+      <button className="btn btn-outline-danger" onClick={ onLogin }>Login</button>
+    </div>
   )
 }
  
