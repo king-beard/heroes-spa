9f6b3afd07a1666e6be2a31db4415a57bc647a02
king-beard <chucho_herrera10@icloud.com>
2023-08-14 15:01:23 -0600

    list heroes

--- src/heroes/pages/DCPage.jsx
+++ src/heroes/pages/DCPage.jsx
@@ -1,9 +1,13 @@
 import { Fragment } from "react"
+import { HeroesList } from "../components"
 
 export const DCPage = () => {
   return (
     <Fragment>
       <h1>DC Page</h1>
+      <hr />
+
+      <HeroesList publisher="DC Comics"/>
     </Fragment>
   )
 }
--- src/heroes/pages/MarvelPage.jsx
+++ src/heroes/pages/MarvelPage.jsx
@@ -1,9 +1,13 @@
 import { Fragment } from "react"
+import { HeroesList } from "../components"
 
 export const MarvelPage = () => {
   return (
     <Fragment>
       <h1>Marvel Page</h1>
+      <hr />
+
+      <HeroesList publisher="Marvel Comics"/>
     </Fragment>
   )
 }
