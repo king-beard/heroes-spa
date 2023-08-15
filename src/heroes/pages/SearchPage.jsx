101ca70ce4076e1216bc1d0cb07b5b2a3159da3a
king-beard <chucho_herrera10@icloud.com>
2023-08-15 12:35:19 -0600

    design SearchPage

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,9 +1,41 @@
 import { Fragment } from "react"
+import { HeroCard } from "../components"
 
 export const SearchPage = () => {
   return (
     <Fragment>
       <h1>Search Page</h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-5">
+          <h4>Searching</h4>
+          <hr />
+          <form>
+            <input 
+              type="text"
+              className="form-control"
+              placeholder="Search a hero"
+              name="searchText"
+              autoComplete="off"
+            />
+          </form>
+          <button className="btn btn-outline-danger mt-1">Search</button>
+        </div>
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+
+          <div className="alert alert-primary">
+            Search a hero
+          </div>
+          <div className="alert alert-danger">
+            No hero with <b>ABC</b>
+          </div>
+
+          {/* <HeroCard /> */}
+        </div>
+      </div>
     </Fragment>
   )
 }
