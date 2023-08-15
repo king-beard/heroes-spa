a706cc0dccf0bf44de2b2d1dd58a7d76362cbf13
king-beard <chucho_herrera10@icloud.com>
2023-08-15 10:30:49 -0600

    hero card

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 export const HeroCard = ({ hero }) => {
   const heroImageUrl = `/assets/heroes/${ hero.id }.jpg`
   return (
@@ -11,7 +13,11 @@ export const HeroCard = ({ hero }) => {
             <div className="card-body">
               <h5 className="card-title">{ hero.superhero }</h5>
               <p className="card-text">{ hero.alter_ego }</p> 
-              <p className="card-text">{ hero.characters }</p> 
+              { hero.alter_ego != hero.characters && <p className="card-text">{ hero.characters }</p>  }
+              <p className="card-text">
+                <small className="text-muted">{ hero.first_appearance }</small>
+              </p> 
+              <Link to={`/hero/${ hero.id }`}>More ...</Link>
             </div>
           </div>
         </div> 
