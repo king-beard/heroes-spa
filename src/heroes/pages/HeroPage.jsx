7e8f73dfdc4143a9d9c20ae1475b1a4460b26917
king-beard <chucho_herrera10@icloud.com>
2023-08-15 10:44:44 -0600

    read arguments by url

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,9 +1,14 @@
 import { Fragment } from "react"
+import { useParams } from "react-router-dom"
+import { getHeroById } from "../helpers"
 
 export const HeroPage = () => {
+  const { id } = useParams() 
+  const hero = getHeroById( id )
+
   return (
     <Fragment>
-      <h1>Hero Page</h1>
+      <h1>Hero Page </h1>
     </Fragment>
   )
 }
