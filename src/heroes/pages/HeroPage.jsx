65b8f13eefb5266606fed03927cd0c985ad4fb6b
king-beard <chucho_herrera10@icloud.com>
2023-08-15 12:08:13 -0600

    component heroscreen

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,15 +1,32 @@
-import { Fragment } from "react"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { getHeroById } from "../helpers"
 
 export const HeroPage = () => {
   const { id } = useParams() 
+  const navigate = useNavigate()
   const hero = getHeroById( id )
+  const heroImageUrl = `/assets/heroes/${ hero.id }.jpg`
+
+  const onNavigateBack = () => navigate(-1)
 
   return (
-    <Fragment>
-      <h1>Hero Page </h1>
-    </Fragment>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img className="img-thumbnail" src={ heroImageUrl } alt={ hero.superhero } />
+      </div>
+      <div className="col-8">
+        <h3>{ hero.superhero }</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"><b>Alter ego: </b>{ hero.alter_ego }</li>
+          <li className="list-group-item"><b>Publisher: </b>{ hero.publisher }</li>
+          <li className="list-group-item"><b>First Appearance: </b>{ hero.first_appearance }</li>
+        </ul>
+
+        <h5 className="mt-3">Characters</h5>
+        <p>{ hero.characters }</p>
+        <button className="btn btn-outline-danger" onClick={ onNavigateBack }>back</button>
+      </div>
+    </div>
   )
 }
    
