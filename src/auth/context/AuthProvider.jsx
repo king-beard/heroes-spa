7a8c9b39cea3673b754317fe7e8df00bcdf322e8
king-beard <chucho_herrera10@icloud.com>
2023-08-21 10:23:43 -0600

    logout

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -15,7 +15,6 @@ export const AuthProvider = ( { children } ) => {
 
   const [ authState, dispatch ] = useReducer( authReducer, {}, init )
 
-
   const login = ( name = '' ) => {
     const user = { id : 'ABC', name }
     const action = {
@@ -26,8 +25,14 @@ export const AuthProvider = ( { children } ) => {
     dispatch( action )
   }
 
+  const logout = () => {
+    localStorage.removeItem('user')
+    const action = { type: types.logout }
+    dispatch( action )
+  }
+
   return (
-    <AuthContext.Provider value={{ authState, login  }}>
+    <AuthContext.Provider value={{ authState, login, logout  }}>
       { children }
     </AuthContext.Provider>
   )
