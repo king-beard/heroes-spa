92d345a73282e51f47cb819485152454459dae33
king-beard <chucho_herrera10@icloud.com>
2023-08-22 11:24:46 -0600

    public route

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,12 +3,17 @@ import { Route, Routes } from "react-router-dom"
 import { LoginPage } from "../auth/pages"
 import { HeroesRoutes } from "../heroes/routes"
 import { PrivateRoute } from "./PrivateRoute"
+import { PublicRoute } from "./PublicRoute"
 
 export const AppRouter = () => {
   return (
     <Fragment>
       <Routes>
-        <Route path="login" element={<LoginPage />} />
+        <Route path="login" element={
+          <PublicRoute>
+            <LoginPage />
+          </PublicRoute>
+        } />
         <Route path="/*" element={
           <PrivateRoute>
             <HeroesRoutes />
